ea155824cd336e7e3320afa9d07dbed3f63a8640
hioken <60014036+hioken@users.noreply.github.com>
2024-06-13 05:20:58 +0000

    c9

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+
 class ActiveSupport::TestCase
   # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
@@ -13,5 +14,20 @@ class ActiveSupport::TestCase
   # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
