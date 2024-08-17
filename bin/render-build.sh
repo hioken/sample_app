c774e078f99ba62faf4d0f55318f376c90cbf563
hioken <60014036+hioken@users.noreply.github.com>
2024-08-17 08:20:23 +0000

    a

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
