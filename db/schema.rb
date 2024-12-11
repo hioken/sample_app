4736bd9fd082a5a4a09728fe3c1b73392831916e
hioken <radcloseff@gmail.com>
2024-12-12 07:22:40 +0900

    set up1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_25_031550) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_11_221120) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,31 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_25_031550) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "channel_users", force: :cascade do |t|
+    t.integer "channel_id_id", null: false
+    t.integer "user_id_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id_id"], name: "index_channel_users_on_channel_id_id"
+    t.index ["user_id_id"], name: "index_channel_users_on_user_id_id"
+  end
+
+  create_table "channels", force: :cascade do |t|
+    t.datetime "last_message_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id_id", null: false
+    t.integer "channel_id_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id_id"], name: "index_messages_on_channel_id_id"
+    t.index ["user_id_id"], name: "index_messages_on_user_id_id"
   end
 
   create_table "microposts", force: :cascade do |t|
@@ -75,5 +100,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_25_031550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "channel_users", "channel_ids"
+  add_foreign_key "channel_users", "user_ids"
+  add_foreign_key "messages", "channel_ids"
+  add_foreign_key "messages", "user_ids"
   add_foreign_key "microposts", "users"
 end
